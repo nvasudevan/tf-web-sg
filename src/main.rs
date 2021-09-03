a5b1ff057f9fff749871fdc12e3579979ed45c9c
Naveneetha Vasudevan <naveneetha@yahoo.com>
2021-09-03 12:00:12 +0100

    Send udp message to metric server

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,73 @@
 use chrono::Utc;
 use std::{
-    io::{Read, Write},
+    env,
+    io::{Read, Write, Result},
     net::{TcpListener, TcpStream},
 };
+use tokio::net::UdpSocket;
+use std::net::SocketAddr;
 
-fn handle_connection(mut tcp_str: TcpStream, app_env: &str, app_ver: &str) {
-    let client = tcp_str.peer_addr().unwrap().to_string();
-    println!("connection received from {}", client);
+/// Send an udp message `app_env=X;app_version=Y` to the `udp_server`
+async fn send_udp_msg(udp_server: &str, app_env: &str, app_ver: &str) -> Result<()> {
+    let msg = format!("app_env={};app_version={}", app_env, app_ver);
+    let local_addr: SocketAddr = SocketAddr::V4("0.0.0.0:0".parse().unwrap());
+    let skt = UdpSocket::bind(local_addr).await?;
+    if let Err(e) = skt.send_to(msg.as_bytes(), udp_server).await{
+       eprintln!("=> error writing to udp server: {}", e);
+    }
+
+    Ok(())
+}
+
+fn handle_connection(mut tcp_str: TcpStream, app_env: &str, app_ver: &str) -> Result<String> {
+    let client = tcp_str.peer_addr()?;
+    println!("connection received from {}", client.to_string());
     let mut buf = [0; 1024];
     if let Ok(_) = tcp_str.read(&mut buf) {
-        println!("client msg: {}", String::from_utf8_lossy(&buf[..]));
         let now = Utc::now().format("%Y-%m-%d-%H-%M-%S%.3f");
         let contents = format!("{} {} {}", now, app_env, app_ver);
         let res_hdr = format!("HTTP/1.1 200 OK\r\nContent-Length: {}", contents.len());
         let res = format!("{}\r\n\r\n{}\n", res_hdr, contents);
-        match tcp_str.write(res.as_bytes()) {
-            Ok(_) => {
-                println!("=> response sent to client: {}", client);
-            }
-            Err(e) => {
-                println!(
-                    "=> error writing to client: {}, error: {}",
-                    client,
-                    e.to_string()
-                );
-            }
+        if let Err(e) = tcp_str.write(res.as_bytes()) {
+            eprintln!("=> error writing to client {} ({})", client, e.to_string());
         }
-        tcp_str.flush().unwrap();
+
+        tcp_str.flush()?;
     };
+
+    Ok(client.to_string())
 }
 
-fn main() {
-    let app_env = std::env::var("APP_ENV").expect("APP_ENV environment variable is not set!");
-    let app_ver = std::env::var("APP_VER").expect("APP_VER environment variable is not set!");
-    let bind_addr = "0.0.0.0:8080";
+#[tokio::main]
+async fn main() -> Result<()> {
+    let app_env = env::var("APP_ENV")
+        .expect("APP_ENV environment variable is not set!");
+    let app_ver = env::var("APP_VER")
+        .expect("APP_VER environment variable is not set!");
+    let bind_addr = env::var("BIND_ADDR")
+        .expect("BIND_ADDR environment variable is not set!");
+    let udp_server = env::var("UDP_SERVER")
+        .expect("UDP_SERVER environment variable is not set!");
     println!("=> starting web server on {}", bind_addr);
-    let my_web = TcpListener::bind(bind_addr).unwrap();
 
-    for stream in my_web.incoming() {
+    let tcp_srv = TcpListener::bind(bind_addr)?;
+    for stream in tcp_srv.incoming() {
         match stream {
-            Ok(tcp_str) => handle_connection(tcp_str, &app_env, &app_ver),
+            Ok(tcp_str) => {
+                match handle_connection(tcp_str, &app_env, &app_ver) {
+                    Ok(_) => {
+                        let _ = send_udp_msg(&udp_server, &app_env, &app_ver).await;
+                    }
+                    Err(e) => {
+                        eprintln!("Failed to read from client: {}", e.to_string());
+                    }
+                }
+            }
             Err(e) => {
-                println!("Connection failed: {}", e.to_string());
+                eprintln!("Connection failed: {}", e.to_string());
             }
         }
     }
+
+    Ok(())
 }
